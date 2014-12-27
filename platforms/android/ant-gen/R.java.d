d08f39afa1c455f3cea069f0b8732478da422d74
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-27 15:21:21 +0000

    First Commit added raw project

---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,26 @@
+D:\xampp\htdocs\proyectoII\platforms\android\ant-gen\com\phonegap\helloworld\R.java \
+ : D:\xampp\htdocs\proyectoII\platforms\android\res\drawable\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-hdpi\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-land-hdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-land-ldpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-land-mdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-land-xhdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-port-hdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-port-ldpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-port-mdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-port-xhdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\drawable-xxhdpi\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\res\values\strings.xml \
+D:\xampp\htdocs\proyectoII\platforms\android\res\xml\config.xml \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-land-hdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-land-ldpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-land-mdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-land-xhdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-port-hdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-port-ldpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-port-mdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-port-xhdpi\screen.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\res\drawable-xxhdpi\icon.png \
+D:\xampp\htdocs\proyectoII\platforms\android\ant-build\AndroidManifest.xml \
---
+++ platforms/android/cordovalib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\xampp\htdocs\proyectoII\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
