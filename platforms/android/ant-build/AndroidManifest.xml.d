574b4a620b7c60054225d8a6f8b6fa03106a5986
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-31 16:10:58 +0000

    Tested compass and location, working perfectly

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-D:\xampp\htdocs\PlantsDiary\platforms\android\ant-build\AndroidManifest.xml : \
-D:\xampp\htdocs\PlantsDiary\platforms\android\AndroidManifest.xml \
+C:\xampp\htdocs\PlantsDiary\platforms\android\ant-build\AndroidManifest.xml : \
+C:\xampp\htdocs\PlantsDiary\platforms\android\AndroidManifest.xml \
