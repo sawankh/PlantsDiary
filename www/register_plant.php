7eeefdc8f97cde8f1a5590bd2262c7139a951961
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-31 18:03:25 +0000

    Finished estructure register_plant

--- www/register_plant.php
+++ www/register_plant.php
@@ -9,5 +9,16 @@ $date = $_POST["Date"];
 $observations = $_POST["Observations"];
 $image = $_POST["imageCamara"];
 
+$dbhost = 'localhost';
+$dbname = 'plants';
+$dbuser = 'root';
+$dbpass = '';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+mysql_select_db($dbname, $conn);
+$query = "INSERT INTO $dbname 
+    VALUES ('$name_sci', '$name_com', '$longitude', '$latitude', '$date', '$observations', '$image');";
+mysql_query($query);
+mysql_close();
+header('Location: add_plant.php');
 
 ?>
